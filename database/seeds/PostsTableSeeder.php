7d77a27668e099023a2e2d431e492591b53775e9
Stefano <tepo@hotmail.it>
2021-05-24 16:58:34 +0200

    Definito seeder per tabella posts, tabella popolata con 10 post

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -14,9 +15,14 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker) {
         // ciclo 10 volte per creare 10 post
         for ($i=0; $i < 10; $i++) { 
-           $newPost = new Post;
-           $newPost->title = $faker->sentence(5);
-           $newPost->date = $faker->date('d-m-Y');
+            $newPost = new Post;
+            $newPost->title = $faker->sentence(5);
+            $newPost->date = $faker->date();
+            $newPost->content = $faker->text(300);
+            $newPost->image = $faker->imageUrl(640, 480, 'animals', true);
+            $newPost->published = $faker->numberBetween(0, 1);
+            $newPost->slug = Str::slug($newPost->title, '-');
+            $newPost->save();
         }
     }
 }
