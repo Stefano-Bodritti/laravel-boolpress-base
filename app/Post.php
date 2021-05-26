97830e00da59f5e35d044809cd2430d6f212a8ed
Stefano <tepo@hotmail.it>
2021-05-26 15:40:27 +0200

    Aggiunta relazione nei model tra posts e tags

--- app/Post.php
+++ app/Post.php
@@ -9,9 +9,14 @@ class Post extends Model
     // abilito mass assignment
     protected $guarded = [];
 
-    // definisco relazione one to many tra le tabelle
+    // definisco relazione one to many con tabella comments
     public function comments() {
 
         return $this->hasMany('App\Comment');
+    }
+
+    // definisco relazione many to many con tabella tags
+    public function tags() {
+        return $this->belongsToMany('App\Tag');
     }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -9,5 +9,8 @@ class Tag extends Model
     // abilito mass assignment
     protected $guarded = [];
 
-    
+    // definisco relazione many to many con tabella posts
+    public function posts() {
+        return $this->belongsToMany('App\Post');
+    }
 }
